31a84b0742847bf3f3ca3b2c2973b170ce09a37c
Saied <saied@ou.edu>
2020-05-01 11:24:59 -0400

    Rededign the imag viewer class.

--- src/helpers.py
+++ src/helpers.py
@@ -10,24 +10,9 @@
 
 from __future__ import absolute_import, division, print_function
 
-import os, glob, scipy.misc
-
-try:
-	from sintel_io import *
-	from flowlib import *
-	import pfmutil as pfm
-except:
-	from src.sintel_io import *
-	from src.flowlib import *
-	import src.pfmutil as pfm
+import os, glob
 
 os.environ['TK_SILENCE_DEPRECATION'] = '1'
-MASKS = ('*.jpg', '*.png', '*.flo', '*.dpt', '*.pfm')
-MASK_DESCRIPTION = ('jpeg files', 'png files', 'flow files', 'depth files', 'disparity files')
-IMAGE_TYPES = list(zip(MASK_DESCRIPTION, MASKS))
-ALL_TYPES = [('*.*', 'all files')] + IMAGE_TYPES
-EXTENSIONS = [mask.split('.')[-1] for mask in MASKS]
-EXTENSIONS += ['jpeg']
 
 def getListOfFiles(dirName, masks=['*.png'], sudirectories=False):
 	listOfFile = os.listdir(dirName)
@@ -40,57 +25,6 @@ def getListOfFiles(dirName, masks=['*.png'], sudirectories=False):
 			allFiles += getListOfFiles(fullPath, masks, sudirectories)
 	
 	return allFiles
-
-def open_image (obj, filename):
-	extension = filename.split('.')[-1].lower()
-	if extension in ['png', 'jpg', 'jpeg']:
-		image = scipy.misc.imread(filename)
-		disp_image = image
-	elif extension == 'flo':
-		image = read_flow(filename)
-		disp_image = flow_to_image(image)
-	elif extension == 'dpt':
-		image = depth_read(filename)
-		disp_image = 1. / image
-		obj.dpt_max = np.amax(disp_image)
-		disp_image = disp_image / np.amax(disp_image) * 255
-	elif extension == 'pfm':
-		image = pfm.load(filename)[0]
-		disp_image = image
-	else:
-		try:
-			image = scipy.misc.imread(filename)
-			disp_image = image
-		except:
-			image = np.ones([200,400]) * 255
-			for i in range(200):
-				image[i, 2*i] = 0
-				image[199-i, 398-2*i] = 0
-			disp_image = image
-	
-	return image, disp_image
-	
-def save_image (obj, filename):
-	extension = filename.split('.')[-1].lower()
-	if extension in ['png', 'jpg', 'jpeg']:
-		scipy.misc.imsave(filename, obj.display_image)
-	elif extension == 'flo' and obj.image_format == 'flo':
-		write_flow(image, obj.original_image)
-	elif extension == 'dpt' and len(obj.original_image.shape) == 2:
-		if obj.image_format == 'dpt':
-			depth_write(filename, obj.original_image)
-		else:
-			depth_write(filename, 1. / obj.original_image)
-	elif extension == 'pfm':
-		if obj.image_format == 'dpt':
-			pfm.save(filename, 1. / (obj.original_image / 255 * obj.dpt_max))
-		else:
-			pfm.save(filename, obj.original_image)
-	else:
-		try:
-			scipy.misc.imsave(filename, obj.display_image)
-		except:
-			Print ("Error: Cannot save.")
 	
 
 if __name__ == "__main__":
--- src/simple_image_viewer.py
+++ src/simple_image_viewer.py
@@ -8,6 +8,13 @@
 ##############################################################################
 from __future__ import absolute_import, division, print_function
 
+MASKS = ('*.jpg', '*.png', '*.flo', '*.dpt', '*.pfm')
+MASK_DESCRIPTION = ('jpeg files', 'png files', 'flow files', 'depth files', 'disparity files')
+IMAGE_TYPES = list(zip(MASK_DESCRIPTION, MASKS))
+ALL_TYPES = [('*.*', 'all files')] + IMAGE_TYPES
+EXTENSIONS = [mask.split('.')[-1] for mask in MASKS]
+EXTENSIONS += ['jpeg']
+
 import PIL.Image
 
 try:
@@ -15,6 +22,15 @@ try:
 	CLIPBOARD_FLAG = False
 except:
 	CLIPBOARD_FLAG = False 
+
+try:
+	from sintel_io import *
+	from flowlib import *
+	import pfmutil as pfm
+except:
+	from src.sintel_io import *
+	from src.flowlib import *
+	import src.pfmutil as pfm
 
 try:
 	from helpers import *
@@ -31,7 +47,7 @@ import PIL.ImageTk
 
 class SimpleImageViewer(Frame):
 	def chg_image(self, filename):
-		self.original_image, self.display_image = open_image(self, filename)
+		self.original_image, self.display_image = self.open_image(filename)
 		self.image_format = filename.split('.')[-1].lower()
 		self.image_index = self.image_list.index(filename)
 		sp = filename.split('/')
@@ -58,7 +74,7 @@ class SimpleImageViewer(Frame):
 		filename = filedialog.asksaveasfilename(filetypes = IMAGE_TYPES, defaultextension='.png') 
 		if not filename: return
 		print ('Saving '+filename)
-		save_image (self, filename)
+		self.save_image (filename)
 		
 	def seek_prev(self):
 		if self.image_index > 0:
@@ -87,6 +103,57 @@ class SimpleImageViewer(Frame):
 			return
 		# clipboard.set_image(self.im, format='png')
 		clipboard.copy(self.im)
+
+	def open_image (self, filename):
+		extension = filename.split('.')[-1].lower()
+		if extension in ['png', 'jpg', 'jpeg']:
+			image = scipy.misc.imread(filename)
+			disp_image = image
+		elif extension == 'flo':
+			image = read_flow(filename)
+			disp_image = flow_to_image(image)
+		elif extension == 'dpt':
+			image = depth_read(filename)
+			disp_image = 1. / image
+			self.dpt_max = np.amax(disp_image)
+			disp_image = disp_image / np.amax(disp_image) * 255
+		elif extension == 'pfm':
+			image = pfm.load(filename)[0]
+			disp_image = image
+		else:
+			try:
+				image = scipy.misc.imread(filename)
+				disp_image = image
+			except:
+				image = np.ones([200,400]) * 255
+				for i in range(200):
+					image[i, 2*i] = 0
+					image[199-i, 398-2*i] = 0
+				disp_image = image
+		
+		return image, disp_image
+	
+	def save_image (self, filename):
+		extension = filename.split('.')[-1].lower()
+		if extension in ['png', 'jpg', 'jpeg']:
+			scipy.misc.imsave(filename, self.display_image)
+		elif extension == 'flo' and self.image_format == 'flo':
+			write_flow(image, self.original_image)
+		elif extension == 'dpt' and len(self.original_image.shape) == 2:
+			if self.image_format == 'dpt':
+				depth_write(filename, self.original_image)
+			else:
+				depth_write(filename, 1. / self.original_image)
+		elif extension == 'pfm':
+			if self.image_format == 'dpt':
+				pfm.save(filename, 1. / (self.original_image / 255 * self.dpt_max))
+			else:
+				pfm.save(filename, self.original_image)
+		else:
+			try:
+				scipy.misc.imsave(filename, self.display_image)
+			except:
+				Print ("Error: Cannot save.")
 
 
 	def __init__(self, master=None):
